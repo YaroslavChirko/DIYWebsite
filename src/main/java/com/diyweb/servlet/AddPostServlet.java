7eca53c3ea30ff89ca8abe1b2c303f3583a1e3f3
YaroslavChirko <yarikchirko@gmail.com>
2022-08-24 01:27:43 +0300

    worked on addPostServlet
    should retrieve pictures from request and add hint on  how to add pictures to specific point

--- src/main/java/com/diyweb/servlet/AddPostServlet.java
+++ src/main/java/com/diyweb/servlet/AddPostServlet.java
@@ -1,9 +1,13 @@
 package com.diyweb.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import com.diyweb.misc.UrlPathParameterExtractor;
 import com.diyweb.models.Cathegory;
+import com.diyweb.models.Post;
 import com.diyweb.models.User;
 import com.diyweb.repo.PostRepoInterface;
 import com.diyweb.repo.UserRepoInterface;
@@ -16,6 +20,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import jakarta.ws.rs.Path;
 
 @WebServlet(urlPatterns = "/posts/add-post/*")
@@ -63,7 +68,60 @@ public class AddPostServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
+		//get title from request
+		String title = req.getParameter("title");
+		//get body from request
+		String body = req.getParameter("postBody");
+		//get category from path
+		String  pathCategory = UrlPathParameterExtractor.processPathParameters(getClass(), req.getPathInfo()).get("category");
+		
+		if(title == null || title.trim().equals("")) {
+			resp.sendError(400, "Title was not provided or was empty");
+			return;
+		}
+		
+		if(body == null || title.trim().equals("")) {
+			resp.sendError(400, "Body was not provided or was empty");
+			return;
+		}
+		
+		if(pathCategory == null || pathCategory.trim().equals("")) {
+			resp.sendError(400, "Category empty or is null");
+			return;
+		}
+		
+		Cathegory category = Cathegory.valueOf(pathCategory);
+		if(category != null) {
+			resp.sendError(400, "Category");
+			return;
+		}
+		
+		//get user email, check it and retrieve user
+		String userEmail = (String)req.getSession().getAttribute("userEmail");
+		UUID  userIdentifier = (UUID)req.getSession().getAttribute("userIdentifier");
+		if(userEmail == null || userIdentifier == null || userEmail.equals("")) {
+			resp.sendError(400, "User credentials are either null or empty");
+			return;
+		}
+		
+		User currentUser = userRepo.getUserByEmail(userEmail);
+		if(currentUser == null || currentUser.isVerified()) {
+			resp.sendError(401, "User not found, try with another account");
+			return;
+		}
+		
+		
+		
+		List<String> pictureUrls = new ArrayList<>();
+		Post currentPost = new Post(currentUser, title, category, body, pictureUrls);
+		//save images to some folder and then add paths to them into pictureUrls array
+		String pathToPictures = Thread.currentThread().getContextClassLoader().getResource("").getPath()+"/"+userIdentifier+"/"+currentPost.hashCode();
+		
+		for() {
+			
+		}
+		
+		postRepo.persist(currentPost);
 	}
 
 	
